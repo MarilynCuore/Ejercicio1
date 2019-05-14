899e875f60b5c15d1a0b4bbc0f5e02e4046932f0
Marilyn <marycuorevilla50@gmail.com>
2019-05-14 23:06:58 +0200

    Variables

---
+++ .history/variables_20190514230533.js
@@ -0,0 +1,5 @@
+let nombreCompleto = 'Marilyn Cuore Villarroel';
+let edad = 25;
+let diasDeLaSemana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+let valoDeLaGravedad = 9.8;
+let primerosNumerosImpares = [1,3,7];
